897a5a5e61768b1c2175a40ae63fdcd15bf2f563
Sam Hatley <sam+github@samhatley.com>
2022-10-30 15:49:21 +0000

    comments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from jobsearch import *
 import pandas as pd
 
-page = test_page()
-titles, companies, dates, links = extract_jobs(page)
+# Here: build a program that iterates through scrape_jobs as required for the
+# number of jobs and job titles to fetch
 
-print(titles)
+# Need to do something with the data output: thinking we build it into a dataframe, which
+# keeps a record of jobs (by link?), and does not save jobs that have been seen before.
+
+# If we can build some kind of interface to look through jobs, mark those that've been
+# applied to, etc, that would be best. Total automation is possible, but will take quite
+# a bit more work!
